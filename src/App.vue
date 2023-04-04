e23241451c7d3064fbb64c3ae2af1f1d0b438e35
Bruno Santos <rapazcomumvl@hotmail.com>
2023-04-04 13:44:18 -0300

    Criando calculator em um arquivo

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,90 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { reactive } from 'vue';
+
+const operator = reactive({
+  filtro: '( )',
+  input1: '_',
+  input2:'_',
+})
+
+function soma() {
+  return parseInt(operator.input1) + parseInt(operator.input2) ;
+}
+function sub() {
+  return operator.input1 - operator.input2;
+}
+function mult() {
+  return operator.input1 * operator.input2;
+}
+function div() {
+  return operator.input1 / operator.input2;
+}
+
+const getOperator = () => {
+  const {filtro} = operator;
+  switch (filtro) {
+    case "+":
+      return soma();
+    case "-":
+      return sub();
+    case "*":
+      return mult();
+    case "/":
+      return div();
+  } 
+}
+
+
+function campoA(evento) {
+  operator.input1 = evento.target.value;
+}
+function campoB(evento) {
+  operator.input2 = evento.target.value;
+}
+
+
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="container text-center bg-secondary">
+    <header class="p-4 mt-4">
+      <h1>Calculator VueJS</h1>
+    </header>
+    <form class="form-control bg-light">
+      <div class="row">
+        <div class="col-lg-4 col-sm-6">
+          <input @keyup="campoA" id="campo-A" class="form-control mt-2" placeholder="Digite um número" type="number">
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <input @keyup="campoB" id="campo-B" class="form-control mt-2" placeholder="Digite um número" type="number">
+        </div>
+        <div class="col-lg-3">
+          <select @change="evento => operator.filtro = evento.target.value" class="form-control mt-2 btn btn-primary">
+            <option  value="Escolha a operação">Operações :</option>
+            <option  value="+">+ Somar</option>
+            <option value="-">- Subtrair</option>
+            <option value="*"> * Multiplicar</option>
+            <option value="/">/ Dividir</option>
+          </select>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-8">
+          <span placeholder="Resultado" class="form-control mt-2">Resultado: {{ operator.input1 }} {{ operator.filtro }} {{ operator.input2 }} = {{getOperator()}}</span>
+        </div>
+        <div class="col-lg-3  mt-2" placeholder="Resultado">
+          <button @submit="operator" class="btn btn-danger  form-control">Limpar</button>
+        </div>
+      </div>
+    </form>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.container {
+  max-width: 760px;
+  padding-bottom: 50px;
 }
 </style>
